01fe6e08412bf3d4503d83d2bf13dbf9e0fbaf28
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-04 21:16:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-cy.visit(baseUrl + "/main.html");
-
-cy.get('section').eq(1).then(ans => {
-  expect(ans.text().trim().length).to.gt(0);
-});
-
-cy.get('#quote').then(ans => {
-  expect(ans.text().trim().length).to.gt(0);
-});
 
