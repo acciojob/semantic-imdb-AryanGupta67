2fd108071454114c47eba02b5d6d4bc2f22c308b
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-04 21:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your code here
+() => {
+  cy.visit(baseUrl + "/main.html");
+
+  // Check that the second <section> (index 1) has some non-empty text
+  cy.get('section').eq(1).then(ans => {
+    expect(ans.text().trim().length).to.gt(0);
+  });
+
+  // Check that the element with id="quote" has non-empty text
+  cy.get('#quote').then(ans => {
+    expect(ans.text().trim().length).to.gt(0);
+  });
+}
+
